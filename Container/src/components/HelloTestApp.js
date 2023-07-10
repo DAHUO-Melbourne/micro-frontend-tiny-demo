e057798bb0b3a5beca3177ca97bdc3cda0239e3f
DaHuo-Melbourne <huodamelbourne@gmail.com>
2023-07-10 13:45:13 +1000

    feat: add nextjs

--- Container/src/components/HelloTestApp.js
+++ Container/src/components/HelloTestApp.js
@@ -3,14 +3,13 @@ import React, { useRef, useEffect } from 'react'
 import { useHistory } from 'react-router-dom';
 
 export default () => {
-  console.log('testing-app')
-    const ref = useRef(null);
-    const history = useHistory();
+  const ref = useRef(null);
+  const history = useHistory();
 
-    useEffect(() => {
-        const { onParentNavigate } = mount(ref.current)
-        history.listen(onParentNavigate)
-    }, [])
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current)
+    history.listen(onParentNavigate)
+  }, [])
 
-    return <div ref={ref} />
+  return <div ref={ref} />
 }
